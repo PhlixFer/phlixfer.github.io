efebfbe7f0945187af8f90d18c890187d4fafd27
PhlixFer <jimjoshjiggle@gmail.com>
2022-01-04 17:17:00 +0700

    add initial responsive navbar bs

---
+++ scripts/navbar.js
@@ -0,0 +1,25 @@
+function hamborgMenu() {
+
+  var x = getElementById('navbar');
+
+  // idk how to javascript so here's some yanderedev-level looping
+
+  if (x.className == "navbar") {
+    x.className += " smol";
+  } else {
+    x.className = "navbar";
+  }
+
+  if (x.className == "navbar-item") {
+    x.className += " smol";
+  } else {
+    x.className = "navbar-item";
+  }
+
+  if (x.className == "navbar-text") {
+    x.className += " smol";
+  } else {
+    x.className = "navbar-text";
+  }
+
+}
