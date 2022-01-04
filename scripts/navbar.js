4556b7ec3b7776067f0cd93bf960593c6653b339
PhlixFer <jimjoshjiggle@gmail.com>
2022-01-04 17:37:06 +0700

    regression

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,8 @@
 function borgorMenu() {
 
-  var x = document.getElementById('navbar');
+    // idk how to javascript so here's some yanderedev-level looping
 
-  // idk how to javascript so here's some yanderedev-level looping
+  var x = document.getElementById('navbar');
 
   if (x.className == "navbar") {
     x.className += " smol";
@@ -10,11 +10,15 @@ function borgorMenu() {
     x.className = "navbar";
   }
 
+  var x = document.getElementById('navbar-item');
+
   if (x.className == "navbar-item") {
     x.className += " smol";
   } else {
     x.className = "navbar-item";
   }
+
+  var x = document.getElementById('navbar-text');
 
   if (x.className == "navbar-text") {
     x.className += " smol";
