5b09f8a0f823d46f4e5727eccbfd001f18401392
PhlixFer <jimjoshjiggle@gmail.com>
2022-03-07 14:25:34 +0700

    update light mode check with initialization

--- scripts/light-mode-check.js
+++ scripts/light-mode-check.js
@@ -1,18 +1,34 @@
+// functions for setting theme-color according to 
+
 function activateLightMode() {
   document.querySelector('meta[name="theme-color"]').setAttribute("content", "#e5e5e5");
 }
-
 function activateDarkMode() {
   document.querySelector('meta[name="theme-color"]').setAttribute("content", "#222222");
 }
 
-window.matchMedia("(prefers-color-scheme: light)").addListener(
-  // e => e.matches && activateDarkMode()
-  function(e) {
-    if (e.matches) {
-      activateLightMode()
-    } else {
-      activateDarkMode()
-    }
-  }
-);
+// check if color scheme is light and set conditional to execute the proper function accordingly
+
+const colorSchemeQueryList = window.matchMedia("(prefers-color-scheme: light)");
+const setColorScheme = e => {
+  if (e.matches) {activateLightMode()} else {activateDarkMode()}
+}
+
+// initialize and calls the conditional with addEventListener
+
+setColorScheme(colorSchemeQueryList);
+colorSchemeQueryList.addEventListener("change", setColorScheme);
+
+// old code
+
+// window.matchMedia("(prefers-color-scheme: light)").addEventListener(
+//   "change",
+//   // e => e.matches && activateLightMode()
+//   function(e) {
+//     if (e.matches) {
+//       activateLightMode()
+//     } else {
+//       activateDarkMode()
+//     }
+//   }
+// );
