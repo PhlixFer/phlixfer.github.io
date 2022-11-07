8d5a3b47a54c501029162373b50673c0e7da8bbe
PhlixFer <phlixfermusic@gmail.com>
2022-11-07 21:38:55 +0700

    add fundraising progress bar and edit a few other styles

---
+++ scripts/commissions-tracker.js
@@ -0,0 +1,21 @@
+const commsDone = [
+	{
+		clientName: "lorem",
+		totalPaid: 0,
+	},
+]
+
+const totalToRaise = 1388;
+var total = 0;
+
+commsDone.forEach((item) => {
+	total += item.totalPaid;
+})
+
+var progressed = ( 100 * total ) / totalToRaise;
+
+let progressBar = document.getElementById('progress-bar');
+progressBar.style.width = progressed + '%';
+
+let progressFraction = document.getElementById('progress-fraction');
+progressFraction.innerHTML = "$" + total.toLocaleString('en') + " / $" + totalToRaise.toLocaleString('en');
