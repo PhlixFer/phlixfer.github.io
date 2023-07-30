f87a7c2a99067399bb90f32e31bdaf7219eb1e30
PhlixFer <phlixfermusic@gmail.com>
2023-07-30 20:05:42 +0700

    change navbar support button to patreon

--- scripts/add-common-elements.js
+++ scripts/add-common-elements.js
@@ -59,16 +59,16 @@ var htmlNavbar = `
       <a href="/about">About</a>
       <a href="/get-real">Get Real</a>
       <div class="navbar-icon-container">
-        <a href="https://boosty.to/phlixfer" target="_blank" class="navbar-icon">
-        <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 235.6 292.2" style="enable-background:new 0 0 235.6 292.2;" xml:space="preserve">
-                <g id="b_1_">
-                        <path class="st0" d="M44.3,164.5L76.9,51.6H127l-10.1,35c-0.1,0.2-0.2,0.4-0.3,0.6L90,179.6h24.8c-10.4,25.9-18.5,46.2-24.3,60.9
-		c-45.8-0.5-58.6-33.3-47.4-72.1 M90.7,240.6l60.4-86.9h-25.6l22.3-55.7c38.2,4,56.2,34.1,45.6,70.5
-		c-11.3,39.1-57.1,72.1-101.7,72.1C91.3,240.6,91,240.6,90.7,240.6z"/>
-                </g>
-        </svg>
-          <p>Buy me a coffee!</p>
+        <a href="https://patreon.com/phlixfer" target="_blank" class="navbar-icon">
+          <svg width="100%" height="100%" viewBox="0 0 600 600" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+              <g transform="matrix(1,0,0,1,-2800,-700)">
+                  <g id="patreon-logo-mono" transform="matrix(1,0,0,1,-700,700)">
+                      <rect x="3500" y="0" width="600" height="600" style="fill:none;"/>
+                      <path d="M3635.5,474.76L3697.73,474.76L3697.73,134.92L3635.5,134.92L3635.5,474.76ZM3862.43,134.921C3792.15,134.921 3734.97,192.152 3734.97,262.503C3734.97,332.641 3792.15,389.704 3862.43,389.704C3932.5,389.704 3989.5,332.641 3989.5,262.503C3989.5,192.152 3932.5,134.921 3862.43,134.921Z"/>
+                  </g>
+              </g>
+          </svg>
+          <p>Support me!</p>
         </a>
       </div>
 `;
