ca0ec74ac0217f788622ecfeb7d33ada7c6119bb
PhlixFer <phlixfermusic@gmail.com>
2024-12-03 16:09:28 +0700

    added comments to waiting screen animation code for future-proofing (from myself)

--- scripts/tmp-waiting-screen.js
+++ scripts/tmp-waiting-screen.js
@@ -15,6 +15,7 @@ function setup() {
   
   baseMouseVec = createVector(windowWidth/2, windowHeight/2);
   
+  // localizes text by checking if the user's preferred language matches an available one, otherwise fall back to english
   if (navigator.language == "de" || navigator.language == "de-DE") {
     titleText = "bitte warte, dinge passieren!";
   } else {
@@ -26,9 +27,12 @@ function draw() {
   clear();
   // background("#bdcb93");
   
+  // interpolates the virtual mouse position with the actual mouse position over time, giving movement a more pleasing "delayed" effect
   baseMouseVec.set(lerp(baseMouseVec.x, mouseX, 0.05), lerp(baseMouseVec.y, mouseY, 0.05));
   let mouseVec = createVector((baseMouseVec.x * 2 - windowWidth) * 0.02, (baseMouseVec.y * 2 - windowHeight) * 0.02);
   
+  // a parallax cursor-following effect is achieved by simply taking the mouse position, shifting the coordinates to the center for alignment, scaling it by varying amounts per "layer", and adding it to the position of each respective element. oh, and everything slowly rotates over time.
+// the various if statements simply switch between slightly different positioning and scaling for smaller layouts
   push();
 
   squareRotation += PI * 0.0008;
@@ -87,9 +91,7 @@ function squareStar(points, radius, thickness) {
   let iter = 0;
   
   while (iter < points) {
-    
     rotate(TAU / points);
-    
     // draws a rectangle center-left aligned to the origin so the rotation creates a star shape!
     quad(0, -thickness/2, radius, -thickness/2, radius, thickness/2, 0, thickness/2);
     
